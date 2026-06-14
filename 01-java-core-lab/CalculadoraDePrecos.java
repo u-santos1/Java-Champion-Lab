b5c6a9ced8c7434900ae4eeeb8831f844a300656
Shiver1155 <hif7f6dyfdt@gmail.com>
2026-06-13 23:15:38 -0300

    feat(core): estrutura fundacao do lab e testes de records/switch
    - Cria estrutura modular de pastas e arquivos README / CONTEXTO_IA
    - Configura pom.xml base do modulo 01 exigindo Java 17
    - Implementa AgendamentoRecord para validar DTOs imutaveis (Java 14+)
    - Cria CalculadoraDePrecos para testar Switch Expressions com retorno direto

---
+++ 01-java-core-lab/CalculadoraDePrecos.java
@@ -0,0 +1,21 @@
+public class CalculadoraDePrecos {
+
+    public static double calcularValorComDesconto(String diaDaSemana, double valorBruto){
+
+        double taxaMultiplicadora = switch (diaDaSemana.toLowerCase()){
+            case "segunda", "terca" -> 0.8;
+            case "quarta", "quinta", "sexta" -> 1.0;
+            case "sabado", "domingo" -> 1.2;
+            default -> throw new IllegalStateException("Dia da semana invalido: " + diaDaSemana);
+        };
+        return valorBruto + taxaMultiplicadora;
+    }
+
+    public static void main(String[] args) {
+        double valorCorte = 45.0;
+
+        System.out.println("Corte na Terça-feira: R$ " + calcularValorComDesconto("terca", valorCorte));
+        System.out.println("Corte na Sexta-feira: R$ " + calcularValorComDesconto("sexta", valorCorte));
+        System.out.println("Corte no Sábado: R$ " + calcularValorComDesconto("sabado", valorCorte));
+    }
+}
